d9df2a35a5d64ba8ccf8f448b8c0e67e70c9308d
Hemanthmon <hemmon963@gmail.com>
2024-06-09 22:34:57 +0530

    update

--- app.js
+++ app.js
@@ -1,29 +1,34 @@
-// Hide the loader after the page has fully loaded
 window.addEventListener('load', function() {
-    document.querySelector('.loader').style.display = 'none';
+  const loader = document.querySelector('.loader');
+  if (loader) {
+      loader.style.display = 'none';
+  }
 });
 
-const toggleBtn = document.getElementById('toggle_btn');
-const dropDownMenu = document.querySelector('.dropdown_menu');
+document.addEventListener('DOMContentLoaded', function() {
+  const toggleBtn = document.getElementById('toggle_btn');
+  const dropDownMenu = document.querySelector('.dropdown_menu');
 
-toggleBtn.onclick = function () {
-    dropDownMenu.classList.toggle('open');
-};
+  if (toggleBtn && dropDownMenu) {
+      toggleBtn.onclick = function () {
+          dropDownMenu.classList.toggle('open');
+      };
+  }
 
-document.addEventListener("DOMContentLoaded", function() {
-    const div = document.querySelector(".text");
-    const text = "Embark on a tranquil journey at our Kingsukh Guest House, enveloped by the scenic allure of Biharinath Hill, Baranti Hill, Susunia Hill, Joychandi Hill, Garhpanchkot, Baranti Dam, Maithon Dam, and the captivating Panchat Dam. Revel in the embrace of comfort, relish delightful meals, and unwind in our verdant garden Oasis. Your ideal retreat beckons, promising a harmonious blend of nature's beauty and heartfelt hospitality. Explore the hidden gems of Purulia, creating memories that linger long after your stay.";
+  const div = document.querySelector(".text");
+  const text = "Embark on a tranquil journey at our Kingsukh Guest House, enveloped by the scenic allure of Biharinath Hill, Baranti Hill, Susunia Hill, Joychandi Hill, Garhpanchkot, Baranti Dam, Maithon Dam, and the captivating Panchat Dam. Revel in the embrace of comfort, relish delightful meals, and unwind in our verdant garden Oasis. Your ideal retreat beckons, promising a harmonious blend of nature's beauty and heartfelt hospitality. Explore the hidden gems of Purulia, creating memories that linger long after your stay.";
   
-    function textTypingEffect(element, text, i = 0) {
+  if (div) {
+      textTypingEffect(div, text);
+  }
+
+  function textTypingEffect(element, text, i = 0) {
       if (i === 0) {
-        element.textContent = "";
+          element.textContent = "";
       }
       if (i < text.length) {
-        element.textContent += text[i];
-        setTimeout(() => textTypingEffect(element, text, i + 1), 10);
+          element.textContent += text[i];
+          setTimeout(() => textTypingEffect(element, text, i + 1), 10);
       }
-    }
-  
-    textTypingEffect(div, text);
-  });
-  
+  }
+});
